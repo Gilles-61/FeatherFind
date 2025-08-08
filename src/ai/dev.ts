0d550ba75be122d0389da03cefe38727716c17b7
Gilles Nobert <GillesNobert@gmail.com>
2025-08-08 23:46:08 +0000

    did you do a checkup for duplikate files folders,a cleanup

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -1,6 +1,5 @@
 import { config } from 'dotenv';
 config();
 
-import '@/ai/flows/suggest-birds.ts';
 import '@/ai/flows/guess-bird-from-description.ts';
 import '@/ai/flows/guess-bird-from-photo.ts';
