0cf0d3c2aa70541e87f8737912d6e93439e18365
Gilles Nobert <GillesNobert@gmail.com>
2025-08-09 01:56:52 +0000

    good can we deploy now

---
+++ src/next.config.ts
@@ -0,0 +1,24 @@
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
